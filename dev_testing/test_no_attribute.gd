5fac65cd210df842044c4e8b4cf5a885d690770f
neth392 <cneth97@gmail.com>
2024-10-29 13:02:15 -0400

    testing cleanup

--- dev_testing/test_no_attribute.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func _ready() -> void:
-	var test1: TestResource = load("res://dev_testing/test_resource.tres")
-	test1.test5 = false
-	print("next!")
-	
-	var test2: TestResource = TestResource.new()
-	print("STOP")
