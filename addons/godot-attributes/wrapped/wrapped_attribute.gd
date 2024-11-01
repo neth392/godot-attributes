49996062431962f6a0c3a3a1798c1a5498fc986e
neth392 <cneth97@gmail.com>
2024-11-01 13:05:36 -0400

    dev

--- addons/godot-attributes/wrapped/wrapped_attribute.gd
+++ addons/godot-attributes/wrapped/wrapped_attribute.gd
@@ -2,3 +2,27 @@
 ## determines the range this attribute's current & base values can live within.
 @tool
 class_name WrappedAttribute extends Attribute
+
+@export_group("Minimums")
+
+@export_subgroup("Base Value")
+
+@export var base_value_minimum: Attribute
+@export var base_value_minimum_value: Attribute.Value
+
+@export_subgroup("Current Value")
+
+@export var current_value_minimum: Attribute
+@export var current_value_minimum_value: Attribute.Value
+
+@export_group("Maximums")
+
+@export_subgroup("Base Value")
+
+@export var base_value_maximum: Attribute
+@export var base_value_maximum_value: Attribute.Value
+
+@export_subgroup("Current Value")
+
+@export var current_value_maximum: Attribute
+@export var current_value_maximum_value: Attribute.Value
