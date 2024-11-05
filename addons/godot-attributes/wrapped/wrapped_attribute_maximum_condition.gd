6a20a8175b2932efb8a4423ac9190f963ae72c9b
neth392 <cneth97@gmail.com>
2024-11-05 14:26:51 -0500

    work

--- addons/godot-attributes/wrapped/wrapped_attribute_maximum_condition.gd
+++ addons/godot-attributes/wrapped/wrapped_attribute_maximum_condition.gd
@@ -8,7 +8,7 @@ func _meets_condition(attribute: Attribute, active: ActiveAttributeEffect) -> bo
 	
 	if active.get_effect().is_permanent():
 		return attribute.has_base_max() \
-		and attribute.get_base_max_value() >= active.get_pending_final_attribute_value()
+		and attribute.get_base_max_value() >= active.get_pending_raw_attribute_value()
 	else:
 		return attribute.has_current_max() \
-		and attribute.get_current_max_value() >= active.get_pending_final_attribute_value()
+		and attribute.get_current_max_value() >= active.get_pending_raw_attribute_value()
--- addons/godot-attributes/wrapped/wrapped_attribute_validator.gd
+++ addons/godot-attributes/wrapped/wrapped_attribute_validator.gd
@@ -1,9 +1,32 @@
+## A validator which can only be applied to [WrappedAttribute]s. Ensures that
+## the base & current values never exceed their set min & max.
 class_name WrappedAttributeValidator extends AttributeValueValidator
 
-
 func _validate(attribute: Attribute, value: float, value_type: Attribute.Value) -> float:
+	assert(attribute is WrappedAttribute, "%s not of type WrappedAttribute" % attribute)
+	# Base value
 	if value_type == Attribute.Value.BASE_VALUE:
-		pass
+		# Base min
+		if attribute.has_base_min():
+			var base_min: float = attribute.get_base_min_value()
+			if value < base_min:
+				return base_min
+		# Base max
+		if attribute.has_base_max():
+			var base_max: float = attribute.get_base_max_value()
+			if value > base_max:
+				return base_max
+		return value
+	# Current value
 	else:
-		pass
-	return value
+		# Current min
+		if attribute.has_current_min():
+			var current_min: float = attribute.get_current_min_value()
+			if value < current_min:
+				return current_min
+		# Current max
+		if attribute.has_current_max():
+			var current_max: float = attribute.get_current_max_value()
+			if value > current_max:
+				return current_max
+		return value
